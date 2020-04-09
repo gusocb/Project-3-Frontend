bd7c728a57e9ad57ab4905daaaea365e29e06f60
Gustavo Cruz <gustavocruzb@outlook.com>
2020-04-08 20:34:59 -0500

    signup component working

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ import ProductList from './components/Products/ProductList'
 import ProductDetail from './components/Products/ProductDetail'
 import ProductUpdate from './components/Products/ProductUpdate'
 import SalesSearch from './components/sales/SalesSearch'
+import Signup from './components/auth/Signup';
+
 
 function App() {
   return (
     <Router>
       <div className='container'>
+        <Route exact path="/signup" component={Signup}/>
         <NavBar/>
         <Route path='/' exact component={Home} />
         <Route path='/products' exact component={ProductList} />
