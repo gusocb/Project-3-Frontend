b402f006b3cf3cb61036c5caf3805bf67af8c2fa
Gustavo Cruz <gustavocruzb@outlook.com>
2020-04-09 19:51:00 -0500

    athentication completed but not working properly

--- src/App.js
+++ src/App.js
@@ -12,31 +12,71 @@ import ProductDetail from './components/Products/ProductDetail'
 import ProductUpdate from './components/Products/ProductUpdate'
 import SalesSearch from './components/sales/SalesSearch'
 import Signup from './components/auth/Signup';
+import AuthService from './components/auth/auth-services';
+import Login from './components/auth/Login';
+
 
 
 function App() {
 
-  const [loggedInUser,setLoggedInUser] = useState(null)
+  const [loggedInUser,setLoggedInUser] = useState(null);
 
+  const service = new AuthService();
+
+  const fetchUser = () => {
+    if(loggedInUser === null) {
+      service.loggedin()
+      .then(res => {
+        setLoggedInUser(res)
+      })
+      .catch( err => {
+        setLoggedInUser(false)
+      })
+    }
+  }
+
+  
   const getTheUser = (userObj) => {
     setLoggedInUser(userObj)
   }
-
-  return (
-    <Router>
-      <div className='container'>
-        <Switch>
-          <NavBar/>
-          <Route exact path="/signup" render={<Signup getUser={getTheUser}/>}/>
-          <Route path='/' exact component={Home} />
-          <Route path='/products' exact component={ProductList} />
-          <Route path='/products/detail/:id' exact component={ProductDetail} />
-          <Route path='/products/update/:id' exact component={ProductUpdate} />
-          <Route path='/sales' exact component={SalesSearch} />
-        </Switch>
-      </div>
-    </Router>
-  );
+  
+  fetchUser()
+  
+  if(loggedInUser){
+    return (
+      <Router>
+        <div className='container'>
+          <Switch>
+            <NavBar userInSession={loggedInUser}/>
+            <Route exact path="/signup" render={<Signup getUser={getTheUser}/>}/>
+            <Route exact path='/login' render={() => <Login getUser={getTheUser}/>}/>
+            <Route path='/' exact component={Home} />
+            <Route path='/products' exact component={ProductList} />
+            <Route path='/products/detail/:id' exact component={ProductDetail} />
+            <Route path='/products/update/:id' exact component={ProductUpdate} />
+            <Route path='/sales' exact component={SalesSearch} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+  else {
+    return(
+      <Router>
+        <div className='container'>
+          <Switch>
+            <NavBar userInSession={loggedInUser}/>
+            <Route exact path="/signup" render={<Signup getUser={getTheUser}/>}/>
+            <Route path='/' exact component={Home} />
+            <Route path='/products' exact component={ProductList} />
+            <Route path='/products/detail/:id' exact component={ProductDetail} />
+            <Route path='/products/update/:id' exact component={ProductUpdate} />
+            <Route path='/sales' exact component={SalesSearch} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,35 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
 import 'bulma/css/bulma.css';
 
 const NavBar = () => {
-    return(
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="https://github.com/gusocb">
-                    <img src="./images/logopos.png" alt="Ironhack Point of Sale"/>
-                </a>
-            </div>
 
-            <div className="navbar-menu">
-                <div className="navbar-start">
-                    <Link className="navbar-item" to='/'>Home</Link>
-                    <Link className="navbar-item" to='/products'>Products</Link>
-                    <Link className="navbar-item" to='/sales'>Sales</Link>
+    const [loggedInUser, setLoggedInUser] = useState(null)
+
+    const componentWillReceiveProps = nextProps => {
+        setLoggedInUser(nextProps['userInSession'])
+    }
+
+    if(loggedInUser){
+        return(
+            
+            <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="https://github.com/gusocb">
+                        <img src="./images/logopos.png" alt="Ironhack Point of Sale"/>
+                    </a>
                 </div>
-
-                <div className="navbar-end">
-                    <div className="navbar-item">
-                        <div className="buttons">
-                            <Link className="button is-danger" to='/logout'>
-                                Logout
-                            </Link>
+    
+                <div className="navbar-menu">
+                    <div className="navbar-start">
+                        <Link className="navbar-item" to='/'>Home</Link>
+                        <Link className="navbar-item" to='/products'>Products</Link>
+                        <Link className="navbar-item" to='/sales'>Sales</Link>
                     </div>
-      </div>
+    
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <Link className="button is-danger" to='/logout'>
+                                    Logout
+                                </Link>
+                        </div>
+          </div>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        )
+    }
+    else {
+        return(
+            <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="https://github.com/gusocb">
+                        <img src="./images/logopos.png" alt="Ironhack Point of Sale"/>
+                    </a>
+                </div>
+    
+                <div className="navbar-menu">
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <Link className="button is-link is-outlined" to='/login'>
+                                    Login
+                                </Link>
+                                <Link className="button is-link" to='/signup'>
+                                    Signup
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
 
-    )
 };
  export default NavBar;
